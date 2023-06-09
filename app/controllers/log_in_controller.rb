6af915cea5ca59fba233787c4a8152c378978857
HabibJR <smhabib.abir2030@gmail.com>
2023-06-09 22:41:11 +0200

    add navbar, footer , signup and login pages

---
+++ app/controllers/log_in_controller.rb
@@ -0,0 +1,7 @@
+class LogInController < ApplicationController
+    def logIn_page
+    end
+
+    def logIn_page
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  get "log_in", to: "log_in#logIn_page"
+  get "sign_up", to: "log_in#signUp"
 end
